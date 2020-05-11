4047b355cf314cd3612dcc4d1fbb51b17ae23ed8
shanthu-hammer <55648960+shanthu-hammer@users.noreply.github.com>
2020-05-11 09:01:54 +0800

    connecting the templates

--- mysite/urls.py
+++ mysite/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
     path('mywork/', include('mywork.urls')),
     path('admin/', admin.site.urls)
      
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] #+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
