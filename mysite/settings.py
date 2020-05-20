3a989274951b55841fc2fa2f899022d8fadaaca9
shanthu-hammer <55648960+shanthu-hammer@users.noreply.github.com>
2020-05-20 16:14:40 +0800

    first commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'j2td43)jj_dzo$xd#pc*5$s3r#4dqns!y1^&#+^xac6oz2t!@@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['shanthu.herokuapp.com/','127.0.0.1']
 
 
 # Application definition
